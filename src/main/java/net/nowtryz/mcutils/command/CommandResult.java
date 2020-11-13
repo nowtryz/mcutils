ecf785a2c15263ea8b0f29512544487806648c7a
Nowtryz <nowtryz.dev@gmail.com>
2020-11-13 21:59:49 +0100

    add command completion, non-static execution, and reload capabilities for CommandManager

--- src/main/java/net/nowtryz/mcutils/command/CommandResult.java
+++ src/main/java/net/nowtryz/mcutils/command/CommandResult.java
@@ -1,5 +1,8 @@
 package net.nowtryz.mcutils.command;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public enum CommandResult {
     /**
      * The error succeeded without error
@@ -17,11 +20,20 @@ public enum CommandResult {
     /**
      * The command failed because the sender is not a player
      */
+    @Deprecated
     NOT_A_PLAYER(true, false),
     /**
      * The command failed because the sender is not the console
+     * @deprecated use {@link CommandResult#WRONG_TARGET} instead
      */
+    @Deprecated
     NOT_A_CONSOLE(true, false),
+    /**
+     * The command failed because the sender was not in the specified targeted senders list.
+     * This is a sort of super-type of {@link CommandResult#NOT_A_CONSOLE} and {@link CommandResult#NOT_A_PLAYER} which
+     * are deprecated. You can catch those three to handle any wrong target call.
+     */
+    WRONG_TARGET(true, false),
 
     /**
      * An unexpected exception occurred and has been logged
@@ -48,11 +60,6 @@ public enum CommandResult {
      * Does this command succeeded
      */
     boolean success;
-
-    CommandResult(boolean valid, boolean success) {
-        this.valid = valid;
-        this.success = success;
-    }
 
     /**
      * The boolean that should be returned to bukkit
