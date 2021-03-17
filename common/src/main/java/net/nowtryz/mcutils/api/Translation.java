4418e1cd0ad9baf4125bd515ca2a0117d45c6329
Nowtryz <nowtryz.dev@gmail.com>
2021-03-17 22:36:32 +0100

    add message formatting utilities

--- common/src/main/java/net/nowtryz/mcutils/api/Translation.java
+++ common/src/main/java/net/nowtryz/mcutils/api/Translation.java
@@ -1,10 +1,43 @@
 package net.nowtryz.mcutils.api;
 
+import lombok.NonNull;
 import org.bukkit.command.CommandSender;
 
+import java.text.MessageFormat;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public interface Translation {
+    Pattern INDEXED = Pattern.compile(".*\\{[^}]+}.*");
+    Pattern INDEXABLE = Pattern.compile("\\{}");
+
+    static MessageFormat parseToMessageFormat(@NonNull String key, @NonNull String message) {
+        try {
+            if (message.contains("{}")) {
+                if (!Translation.INDEXED.matcher(message).matches()) {
+                    Matcher matcher = Translation.INDEXABLE.matcher(message);
+                    StringBuffer sb = new StringBuffer();
+                    for (int i=0; matcher.find(); i++) {
+                        matcher.appendReplacement(sb, "{" + i + "}");
+                    }
+                    matcher.appendTail(sb);
+                    return new MessageFormat(sb.toString());
+                } else {
+                    throw new IllegalArgumentException(key + " mixes non-indexed ({}) and indexed ({n}) parameters");
+                }
+            } else {
+                return new MessageFormat(message);
+            }
+        } catch (IllegalArgumentException exception) {
+            if (exception.getCause() != null) {
+                throw new IllegalArgumentException("Could not parse " + key, exception.getCause());
+            } else {
+                throw new IllegalArgumentException("Could not parse " + key, exception);
+            }
+        }
+    }
+
     /**
      * Gets the translated message
      * @return the translated message
